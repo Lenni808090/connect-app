82421df8fb4f2dd9fa61360460e4577f36cceb66
Lenni <haselenni@gmail.com>
2025-04-29 22:19:31 +0200

    ka

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-
+import Room from "../models/room.model.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -12,20 +12,63 @@ const io = new Server(server, {
     }
 });
 
-export function getReceiverSocketId(userId){
-    return userSocketMap[userId];
-}
-
-//used to store online users
-const userSocketMap = {};// {userId: socketId}
-
-
 io.on("connection", (socket) => {
     console.log("a user connected", socket.id);
+
+    socket.on("join_room", async (roomId) => {
+        socket.join(roomId);
+        const room = await Room.findOne({ roomId });
+        if (room) {
+            io.to(roomId).emit('player_joined', { players: room.players });
+        }
+    });
+
+    socket.on("game_started", async ({ roomId }) => {
+        const room = await Room.findOne({ roomId });
+        if (room) {
+            io.to(roomId).emit('game_update', {
+                gameState: room.gameState,
+                currentCategory: room.currentCategory,
+                submissions: room.submissions,
+                currentScore: room.currentScore,
+                round: room.round
+            });
+        }
+    });
+
+    socket.on("word_submitted", async ({ roomId }) => {
+        const room = await Room.findOne({ roomId });
+        if (room) {
+            io.to(roomId).emit('game_update', {
+                gameState: room.gameState,
+                currentCategory: room.currentCategory,
+                submissions: room.submissions,
+                currentScore: room.currentScore,
+                round: room.round
+            });
+        }
+    });
+
+    socket.on("round_ended", async ({ roomId }) => {
+        const room = await Room.findOne({ roomId });
+        if (room) {
+            io.to(roomId).emit('game_update', {
+                gameState: room.gameState,
+                currentCategory: room.currentCategory,
+                submissions: room.submissions,
+                currentScore: room.currentScore,
+                round: room.round
+            });
+        }
+    });
+
+    socket.on("leave_room", (roomId) => {
+        socket.leave(roomId);
+    });
 
     socket.on("disconnect", () => {
         console.log("user disconnected");
     });
 });
 
-export { io, server, app}
+export { io, server, app };
