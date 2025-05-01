f1c749aaff5d82bd5ff75be3412a91519307432d
Lenni <haselenni@gmail.com>
2025-05-01 20:12:37 +0200

    nocxh mehr ich geh crashout

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://connect-app-api.vercel.app"],
+    origin: ["http://localhost:5173", "https://connect-app-khaki.vercel.app"],
     credentials: true
   },
 });
--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,12 +1,17 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5001");
+const socket = io(import.meta.env.NODE_ENV === 'production' 
+    ? "https://connect-app-api.vercel.app" 
+    : "http://localhost:5001", {
+  withCredentials: true,
+  autoConnect: false
+});
 
 socket.on("connect", () => {
-    const userId = localStorage.getItem('userId');
-    if (userId) {
-        socket.emit("register_user", userId);
-    }
+  const userId = localStorage.getItem("userId");
+  if (userId) {
+    socket.emit("register_user", userId);
+  }
 });
 
 export default socket;
