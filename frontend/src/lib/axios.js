f009db928ec093f06d3223a1f49f125346981669
Lenni <haselenni@gmail.com>
2025-05-01 20:05:40 +0200

    jetzt vieleichjt?

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.NODE_ENV === 'production' 
         ? "https://connect-app-api.vercel.app/api" 
-        : "http://localhost:3000/api",
+        : "http://localhost:5001/api",
     withCredentials: true,
 });
